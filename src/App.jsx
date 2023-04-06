156bcb683104e65456eb0aa356c33154b5f563f5
olmos0901 <olmosbek9933@gmail.com>
2023-04-06 19:14:21 +0500

    react_shablon

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Haader/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Haader/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="nav">
+          <a href="#" className="nav__brand">
+            REACT.JS
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
